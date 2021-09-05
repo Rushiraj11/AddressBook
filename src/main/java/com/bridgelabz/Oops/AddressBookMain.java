aed21ebaad1ed8426e25e5f7d2b93ab55ccd86df
Rushiraj11 <grushiraj95@gmail.com>
2021-09-05 22:54:10 +0530

    Added: Creating Contact Details

--- src/main/java/com/bridgelabz/Oops/AddressBookMain.java
+++ src/main/java/com/bridgelabz/Oops/AddressBookMain.java
@@ -1,8 +1,32 @@
 package com.bridgelabz.Oops;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class AddressBookMain {
 
     public static void main(String[] args) {
-        System.out.println("Welcome to Address book Program");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Welcome to Address Book Program");
+        System.out.println("Enter the First Name");
+        String firstName = sc.next();
+        System.out.println("Enter the Last Name");
+        String lastName = sc.next();
+        System.out.println("Enter The Address");
+        String address = sc.next();
+        System.out.println("Enter the city");
+        String city = sc.next();
+        System.out.println("Enter the state");
+        String state = sc.next();
+        System.out.println("Enter the zip Code");
+        int zip = sc.nextInt();
+        System.out.println("Enter the Phone number");
+        long phone = sc.nextLong();
+        List<ContactDetails> contactList = new ArrayList<>();
+        ContactDetails contact = new ContactDetails(firstName, lastName, address, city, state, zip, phone);
+        contactList.add(contact);
+
     }
+
 }
+
